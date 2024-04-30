9cfd7b4d4236e03b6a3f317ac0968386efd9ca44
Faizan Khan <71210312+ATOMICMAN007@users.noreply.github.com>
2024-04-30 17:49:55 +0530

    Update app.py for proper URL redirects

--- app.py
+++ app.py
@@ -37,3 +37,26 @@ def index():
         return render_template('index.html', short_url=short_url)
 
     return render_template('index.html')
+
+@app.route('/<id>')
+def url_redirect(id):
+    conn = get_db_connection()
+
+    original_id = hashids.decode(id)
+    if original_id:
+        original_id = original_id[0]
+        url_data = conn.execute('SELECT original_url, clicks FROM urls'
+                                ' WHERE id = (?)', (original_id,)
+                                ).fetchone()
+        original_url = url_data['original_url']
+        clicks = url_data['clicks']
+
+        conn.execute('UPDATE urls SET clicks = ? WHERE id = ?',
+                     (clicks+1, original_id))
+
+        conn.commit()
+        conn.close()
+        return redirect(original_url)
+    else:
+        flash('Invalid URL')
+        return redirect(url_for('index'))
